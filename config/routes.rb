6519a2004f5065976038606ae114ae821b80f20e
Joyce Wong <wcyjoyce.hk@gmail.com>
2017-12-20 23:28:04 +0800

    HTML and CSS for navbar done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
+
   resources :posts, only: [:index, :create, :show, :edit, :update, :destroy]
+
+  get "users/:id/dashboard", to: "users#dashboard", as: "dashboard"
 end
