10debe0e6f6795ddba966c005cdf0ebd911b6354
Joyce Wong <wcyjoyce.hk@gmail.com>
2017-12-21 11:32:06 +0800

    added protection for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,10 @@ class PostsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @posts = Post.all
+    @posts = policy_scope(Post).order(created_at: :desc)
   end
 
   def show
-    authorize @post
   end
 
   def new
