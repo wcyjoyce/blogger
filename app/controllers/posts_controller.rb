c01be43e862b7678f456f2a7afc22cc7903a7004
Joyce Wong <wcyjoyce.hk@gmail.com>
2017-12-26 02:46:03 +0800

    added validations for comments in post model and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  skip_before_action :authenticate_user!, only: [:index, :archive, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @posts = policy_scope(Post).order(created_at: :desc)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+
   validates :title, :content, presence: true
 
   # Enables photo uploads
