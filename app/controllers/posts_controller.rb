3607b78b809a17af7a14108d933b6021429adc6c
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-01-05 03:51:27 +0800

    descending order of posts display when using search method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
     if params[:tag]
       @posts = policy_scope(Post).tagged_with(params[:tag]).order(created_at: :desc)
     elsif params[:search].present?
-      @posts = policy_scope(Post).global_search(params[:search])
+      @posts = policy_scope(Post).order(created_at: :desc).global_search(params[:search])
     else
       @posts = policy_scope(Post).order(created_at: :desc)
     end
