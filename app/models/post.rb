7bf2e72f0216aa25e441016b28b5015b77d8e066
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-01-04 23:19:07 +0800

    dependent validation added for post/tagging

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
 
   validates :title, :content, presence: true
