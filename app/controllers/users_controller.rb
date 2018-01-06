e43038365c8e1f06ddbcc5029346f5b7397ef1f3
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-01-06 16:19:42 +0800

    routing to dashboard upon user sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path
+      redirect_to dashboard_path(@user)
     else
       render :new
     end
